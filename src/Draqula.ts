8dbb7ba2d826564a191934142e4637aef2518538
ancmsvn <16853175+quaj@users.noreply.github.com>
2019-09-08 03:39:41 +0700

    fully GraphQLError

--- src/Draqula.ts
+++ src/Draqula.ts
@@ -141,7 +141,12 @@ export default class Draqula {
 				// GraphQL servers don't fail the request, even if resolvers are failing
 				// They still return 200 status code, but add `errors` field in the response
 				if (Array.isArray(response.errors)) {
-					const errors = response.errors.map((error: any) => new Error(error.message));
+					const errors = response.errors.map((error: any) => {
+						let err = new Error(error.message) as any;
+						for (let key in error) {
+							err[key] = error[key];
+						}
+					});
 					throw new GraphQLError(errors);
 				}
 
