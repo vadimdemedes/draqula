8b967eb48f0c9dbe9288a2a3f28bd89af7293912
Víctor Martínez <knoopx@gmail.com>
2019-10-11 05:54:27 +0200

    Fix "Node does not exist" error on mutation (#10)

--- src/Draqula.ts
+++ src/Draqula.ts
@@ -255,11 +255,14 @@ export default class Draqula {
 
 		// Find all queries that have at least one __typename from mutation response
 		for (const typename of typenames) {
-			const dependantQueries = this.graph
-				.dependantsOf(`type:${typename}`)
-				.map(queryId => queryId.replace('query:', ''));
+			const nodeName = `type:${typename}`
+			if (this.graph.hasNode(nodeName)) {
+				const dependantQueries = this.graph
+					.dependantsOf(nodeName)
+					.map(queryId => queryId.replace('query:', ''));
 
-			refetchQueries.push(...dependantQueries);
+				refetchQueries.push(...dependantQueries);
+			}
 		}
 
 		// Custom queries to refetch, specified by `useMutation()`
